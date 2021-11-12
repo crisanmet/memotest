e5cb363133f20972ef3cac6650b603131e5b7e4b
Cristian Sancricca <cristiansancricca@gmail.com>
2021-11-12 16:35:50 -0300

    se crea json de jugadores, se maqueta html y se agregan algunas funciones

---
+++ index.js
@@ -0,0 +1,37 @@
+const $contenedorJugadores = document.querySelector(".contenedor-jugadores");
+const $template = document.querySelector(".template-jugadores").content;
+const fragment = document.createDocumentFragment();
+const $jugadores = document.querySelectorAll(".jugador");
+
+const jugadoresElegidos = [];
+let ronda = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  obtenerJugadores();
+});
+const obtenerJugadores = () => {
+  fetch("./jugadores.json")
+    .then((res) => res.json())
+    .then((jugadores) => renderizarJugadores(jugadores));
+};
+
+const renderizarJugadores = (jugadores) => {
+  const jugadoresConcatenados = jugadores.concat(jugadores);
+  const jugadoresBarajados = barajarJugadores(jugadoresConcatenados);
+
+  jugadoresBarajados.forEach((jugador) => {
+    $template.querySelector(".dorso img").setAttribute("src", jugador.img);
+    $template.querySelector(".frente img").setAttribute("src", jugador.logo);
+    $template.querySelector(".frente img").dataset.nombre = jugador.nombre;
+
+    const clon = $template.cloneNode(true);
+    fragment.appendChild(clon);
+  });
+  $contenedorJugadores.appendChild(fragment);
+};
+
+const barajarJugadores = (jugador) => {
+  return jugador.sort(() => (Math.random() > 0.5 ? 1 : -1));
+};
+
+console.log($jugadores);
