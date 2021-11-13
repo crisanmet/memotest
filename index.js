e51fee789c4caad940572a83586014990000e7c3
Cristian Sancricca <cristiansancricca@gmail.com>
2021-11-13 02:44:19 -0300

    se agregan funciones de eliminar, mostrar jdores

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const $contenedorJugadores = document.querySelector(".contenedor-jugadores");
 const $template = document.querySelector(".template-jugadores").content;
+const $mensajeFinal = document.querySelector(".mensaje-final");
 const fragment = document.createDocumentFragment();
-const $jugadores = document.querySelectorAll(".jugador");
 
-const jugadoresClickeado = [];
+let jugadoresClickeados = [];
 let ronda = 0;
+let aciertos = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
   obtenerJugadores();
@@ -22,7 +23,7 @@ const renderizarJugadores = (jugadores) => {
   jugadoresBarajados.forEach((jugador) => {
     $template.querySelector(".dorso img").setAttribute("src", jugador.img);
     $template.querySelector(".frente img").setAttribute("src", jugador.logo);
-    $template.querySelector(".frente img").dataset.nombre = jugador.nombre;
+    $template.querySelector(".frente img").id = jugador.nombre;
 
     const clon = $template.cloneNode(true);
     fragment.appendChild(clon);
@@ -35,12 +36,58 @@ const barajarJugadores = (jugador) => {
 };
 
 $contenedorJugadores.addEventListener("click", (e) => {
-  const $jugadorClickeado = e.path[0].dataset.nombre;
+  const $jugadorClickeado =
+    e.target.parentElement.parentElement.classList.contains("jugador");
   if ($jugadorClickeado) {
-    chequearJugadores($jugadorClickeado);
+    manejarClickUsuario(e.target);
   }
 });
 
-const chequearJugadores = (e) => {
-  jugadoresClickeado.push(e);
+const manejarClickUsuario = (jugador) => {
+  mostrarJugador(jugador.parentElement.parentElement);
+  jugadoresClickeados.push(jugador);
+
+  if (jugadoresClickeados.length === 2) {
+    evaluarCoincidencia(jugadoresClickeados);
+  }
+};
+
+const mostrarJugador = (jugador) => {
+  jugador.classList.add("rotar-jugador");
+};
+
+const evaluarCoincidencia = (jugadores) => {
+  if (jugadores[0].id === jugadores[1].id) {
+    eliminarJugadores(jugadores);
+  } else {
+    ocultarJugador(jugadores);
+  }
+  ronda++;
+};
+
+const eliminarJugadores = (jugadores) => {
+  jugadores.forEach((e) => {
+    setTimeout(() => {
+      e.parentElement.parentElement.classList.add("acierto");
+      //e.parentElement.parentElement.classList.remove();
+    }, 500);
+  });
+  aciertos++;
+  jugadoresClickeados = [];
+  chequearFinJuego();
+};
+
+const ocultarJugador = (jugadores) => {
+  jugadores.forEach((e) =>
+    setTimeout(() => {
+      e.parentElement.parentElement.classList.remove("rotar-jugador");
+    }, 500)
+  );
+  jugadoresClickeados = [];
+};
+
+const chequearFinJuego = () => {
+  if (document.querySelectorAll(".jugador").length / 2 === aciertos) {
+    $mensajeFinal.textContent = `Felicitaciones! Sos mas bostero que la raulito. Lo completaste en ${ronda} Rondas`;
+  }
 };
